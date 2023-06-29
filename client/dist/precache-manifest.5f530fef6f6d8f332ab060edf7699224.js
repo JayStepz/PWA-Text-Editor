84c48c5d0b93bd84c05318e25d430b8807dbb935
JayStepz <jakob_s_stephens@hotmail.com>
2023-06-29 12:47:47 -0400

    kept forcing audit fixes

---
+++ client/dist/precache-manifest.5f530fef6f6d8f332ab060edf7699224.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3033a2308c3a28c076f1",
+    "url": "autoeditor.bundle.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "autoeditor.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "dba700cf8436c849570900c17929e9ee",
+    "url": "autoindex.html"
+  },
+  {
+    "revision": "8b96e5b2e911deec1a8f",
+    "url": "autoinstall.bundle.js"
+  },
+  {
+    "revision": "cb687b59ec45a5b09d85",
+    "url": "automain.bundle.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "automain.bundle.js.LICENSE.txt"
+  }
+]);
