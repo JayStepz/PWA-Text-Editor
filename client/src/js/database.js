0eb5017b85e772d09c5f2e983381a2e3cfae9b6c
JayStepz <jakob_s_stephens@hotmail.com>
2023-06-29 15:21:16 -0400

    I don't know what I'm doing at this point. I can't get this to work as it should to save my life.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,8 +14,6 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented')
-
   // Create a connection to the database database and version we want to use.
   const contactDb = await openDB('jate', 1);
 
@@ -26,7 +24,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.add(content);
+  const request = store.add({jate: content});
 
   // Get confirmation of the request.
   const result = await request;
@@ -34,7 +32,7 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async (content) => {
+export const getDb = async () => {
   console.error('getDb not implemented')
 
   // Create a connection to the database database and version we want to use.
@@ -47,7 +45,7 @@ export const getDb = async (content) => {
   const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll(content);
+  const request = store.getAll();
 
   // Get confirmation of the request.
   const result = await request;
